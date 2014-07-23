749dbe4a65fa6919e816ec40d224c18ee38d2beb
busz <qiankun90@gmail.com>
2014-07-23 23:41:20 +0800

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -1,3 +1,4 @@
+'''
 Leetcode : Permutations
 
 Given a collection of numbers, return all possible permutations.
@@ -14,4 +15,24 @@ class Solution:
     # @param num, a list of integer
     # @return a list of lists of integers
     def permute(self, num):
+        if len(num) <2:
+            return [num]
+            
+        r = [];
         
+       
+        r = self.permuteList(num)
+        return r
+    def permuteList(self , sortList):
+        if len(sortList) == 1:
+            return [sortList]
+        else:
+            result = [];
+            for i in range(len(sortList)):
+                bak = sortList[:];
+                head = bak.pop(i);
+                for j in self.permuteList(bak):
+                    j.insert(0 , head)
+                    result.append(j)
+            
+            return result
