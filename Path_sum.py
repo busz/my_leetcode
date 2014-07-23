7e08183e915c0cfc0ce4376c869e291246dc4333
busz <qiankun90@gmail.com>
2014-07-24 00:55:35 +0800

    Update Path_sum.py

--- Path_sum.py
+++ Path_sum.py
@@ -21,12 +21,12 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
 @author: xqk
 '''
-# Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     # @param root, a tree node
@@ -35,11 +35,40 @@ class Solution:
     def hasPathSum(self, root, sum):
         if root  == None :
             return False
-        rSum = []
-        level = 0
-        t = [root]
-        tsum = 0
-        while len(t) != 0 and tsum != sum :
-            
-            
+        stack = [];
         
+        stack.append(root);
+        visit = [0];
+        
+        flag = False;
+        while len(stack) != 0 :
+            num = len(stack);
+            if visit[num-1] == 2:
+                visit.pop(num-1);
+                stack.pop(num-1)
+                continue
+            node = stack[num-1];
+            visit[num-1] = visit[num-1] + 1
+            
+            if node.left == None and node.right == None:
+                if node.val == sum:
+                    flag = True;
+                    break;
+                else:
+                    visit[num-1] = 2
+                    continue
+            if visit[num-1] == 1:
+                if node.left != None:
+                    node.left.val = node.left.val + node.val
+                    stack.append(node.left)
+                    visit.append(0);
+            if visit[num-1] == 2:
+                if node.right != None:
+                    node.right.val = node.right.val + node.val
+                    stack.append(node.right)
+                    visit.append(0);
+                    
+            
+            
+            
+        return flag
