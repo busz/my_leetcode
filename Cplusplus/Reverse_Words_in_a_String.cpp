86fb6027c085a61efba0301ca1a7a2b7a88ae950
busz <qiankun90@gmail.com>
2014-08-29 11:05:21 +0800

    Create Reverse_Words_in_a_String.cpp

---
+++ Cplusplus/Reverse_Words_in_a_String.cpp
@@ -0,0 +1,54 @@
+//copy from http://blog.csdn.net/kenden23/article/details/20701069
+
+/*
+
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	void reverseWords(string &s)
+	{
+		string rs;
+		for (int i = s.length()-1; i >= 0; )
+		{
+			while (i >= 0 && s[i] == ' ')
+                i--;
+			if (i < 0) break;
+
+			if (!rs.empty())
+                rs.push_back(' ');
+
+			string t;
+
+			while (i >= 0 && s[i] != ' ')
+                t.push_back(s[i--]);
+
+			reverse(t.begin(), t.end());
+			rs.append(t);
+		}
+		s = rs;
+	}
+};
+
+
+
+int main()
+{
+    Solution test = Solution();
+    string s = "the sky is blue";
+    test.reverseWords(s);
+    std::cout<<s<<std::endl;
+    return 0;
+}
+
